0d960346715af10ae61388e23c66c011691afc91
Ubaidnawal <ubaidnawal857@gmail.com>
2025-12-11 12:59:40 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+function Asnyfuncation() {
+    console.log('sir ap ka time chal gya ');
+
+    setTimeout(() => {
+
+
+        console.log("sir ap ghar chala jaya");
+
+    }, 5000);
+
+        console.log("sir ap ki shadi mubarake");
+
+
+
+
+}
+Asnyfuncation()
